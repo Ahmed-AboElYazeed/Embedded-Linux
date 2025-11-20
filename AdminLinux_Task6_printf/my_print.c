5b8cd10eed78d6c6c226f3e6ce84a8b5594dc321
Ahmed_AboElYazeed <aboelyazeed7777@gmail.com>
2025-11-20 13:16:36 +0200

    printing on the terminal useing registers

--- AdminLinux_Task6_printf/my_print.c
+++ AdminLinux_Task6_printf/my_print.c
@@ -1,9 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
-
-int main()
-{
-    asm
-    {
-        MOV 0x01
+/**
+ * main - Print a string using inline assembly and Linux syscalls.
+ */
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s \"<string>\"\n", argv[0]);
+        return EXIT_FAILURE;
     }
+
+    const char *msg = argv[1];
+    size_t len = strlen(msg);
+
+    // --- print message ---
+    __asm__ __volatile__ (
+        "mov $1, %%rax\n"      // syscall: write
+        "mov $1, %%rdi\n"      // fd = 1 (stdout)
+        "syscall\n"
+        :
+        : "S"(msg), "d"(len)   // S = RSI, d = RDX
+        : "rax", "rdi"
+    );
+
+    // --- print newline ---
+    const char nl = '\n';
+    __asm__ __volatile__ (
+        "mov $1, %%rax\n"      // syscall: write
+        "mov $1, %%rdi\n"      // stdout
+        "syscall\n"
+        :
+        : "S"(&nl), "d"(1)
+        : "rax", "rdi"
+    );
+
+    return EXIT_SUCCESS;
 }
+
--- AdminLinux_Task6_printf/task6.md
+++ AdminLinux_Task6_printf/task6.md
@@ -14,3 +14,10 @@ look at amd assembly instruction set
 
 ![image-20251108093732764](assets/image-20251108093732764.png)
 
+### third:
+
+![image-20251120131508897](assets/image-20251120131508897.png)
+
+### fourth:
+
+![image-20251120131405242](assets/image-20251120131405242.png)
