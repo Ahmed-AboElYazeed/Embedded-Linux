1cb0a17dba11f7beb08f7f0d3993aec698d0012d
Ahmed_AboElYazeed <aboelyazeed7777@gmail.com>
2026-01-07 00:50:25 +0200

    L&Tproject commit 1.2, building LogMannager

--- cppProject/Data_Logging&Telemetry/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zee/ITI_Files/linux/Embedded-Linux/cppProject/Data_Logging&Telemetry
-# Build directory: /home/zee/ITI_Files/linux/Embedded-Linux/cppProject/Data_Logging&Telemetry/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
--- cppProject/Data_Logging&Telemetry/build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zee/ITI_Files/linux/Embedded-Linux/cppProject/Data_Logging&Telemetry/test
-# Build directory: /home/zee/ITI_Files/linux/Embedded-Linux/cppProject/Data_Logging&Telemetry/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/zee/ITI_Files/linux/Embedded-Linux/cppProject/Data_Logging&Telemetry/build/test/LoggingTests[1]_include.cmake")
